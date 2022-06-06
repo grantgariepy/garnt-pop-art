81bc80dd9387fa6b02df3f27620bba2a2dbb103b
grantgariepy <grantgariepy@protonmail.com>
2022-06-05 18:44:29 -0600

    navbar styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import HeroSection from "./components/heroSection/HeroSection";
 import Navbar from "./components/navbar/Navbar";
+
 
 function App() {
   return (
-      <Navbar/>
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
   );
 }
 
---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+import "./herosection.css"
+
+const HeroSection = () => {
+  return (
+    <div>HeroSection</div>
+  )
+}
+
+export default HeroSection
